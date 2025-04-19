dabaf4fe6fd3b2de20bc651063eed9e8260228b8
AdeebaAzam03 <adeebaazam03@gmail.com>
2025-04-20 01:58:36 +0530

    Fix port issue for Render

--- app.py
+++ app.py
@@ -16,5 +16,9 @@ def recommend():
     outfits = get_recommendations(weather, wardrobe)
     return jsonify({'outfits': outfits})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
